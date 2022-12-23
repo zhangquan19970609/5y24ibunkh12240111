eb92fa83889a239ed3f4f0f5757c191385732f28
zhangquan19970609 <zhangquan19970609@outlook.com>
2022-12-24 01:10:54 +0800

    32423commit

---
+++ build/precache-manifest.20d5a1a4da8b3fd197a4984ebbde16be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4396171412856f53c0495f99a9a424c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c95c016048a8ffec045",
+    "url": "/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "f2a4c345f53b8d1c1e32",
+    "url": "/static/js/2.0abff21a.chunk.js"
+  },
+  {
+    "revision": "abc28c365c609ffc9a2e2e165c7e73d8",
+    "url": "/static/js/2.0abff21a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c95c016048a8ffec045",
+    "url": "/static/js/main.2fce9dcb.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/static/media/logo.9a3d2645.svg"
+  }
+]);
